138a6a3070d133ffac4edff0df6ddbdc14699f10
Elias Aebi <eyelash@users.noreply.github.com>
2017-07-25 13:19:45 +0200

    add a circle and a rounded rectangle to the draw demo

--- demos/draw.c
+++ demos/draw.c
@@ -1,6 +1,7 @@
 #include <gral.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#define ROUNDED_CORNER_RATIO .5522847f
 
 struct gral_demo {
 	struct gral_application *application;
@@ -9,6 +10,39 @@ struct gral_demo {
 
 static int close(void *user_data) {
 	return 1;
+}
+
+static void rounded_corner_x(struct gral_draw_context *draw_context, float x, float y) {
+	float current_x, current_y;
+	gral_draw_context_get_current_point(draw_context, &current_x, &current_y);
+	gral_draw_context_curve_to(draw_context, current_x + ROUNDED_CORNER_RATIO * (x - current_x), current_y, x, y + ROUNDED_CORNER_RATIO * (current_y - y), x, y);
+}
+
+static void rounded_corner_y(struct gral_draw_context *draw_context, float x, float y) {
+	float current_x, current_y;
+	gral_draw_context_get_current_point(draw_context, &current_x, &current_y);
+	gral_draw_context_curve_to(draw_context, current_x, current_y + ROUNDED_CORNER_RATIO * (y - current_y), x + ROUNDED_CORNER_RATIO * (current_x - x), y, x, y);
+}
+
+static void add_circle(struct gral_draw_context *draw_context, float x, float y, float size) {
+	float radius = size / 2.f;
+	gral_draw_context_move_to(draw_context, x, y + radius);
+	rounded_corner_y(draw_context, x + radius, y);
+	rounded_corner_x(draw_context, x + size, y + radius);
+	rounded_corner_y(draw_context, x + radius, y + size);
+	rounded_corner_x(draw_context, x, y + radius);
+}
+
+static void add_rounded_rectangle(struct gral_draw_context *draw_context, float x, float y, float width, float height, float radius) {
+	gral_draw_context_move_to(draw_context, x, y+radius);
+	rounded_corner_y(draw_context, x + radius, y);
+	gral_draw_context_line_to(draw_context, x + width - radius, y);
+	rounded_corner_x(draw_context, x + width, y + radius);
+	gral_draw_context_line_to(draw_context, x+width, y+height-radius);
+	rounded_corner_y(draw_context, x + width - radius, y + height);
+	gral_draw_context_line_to(draw_context, x+radius, y+height);
+	rounded_corner_x(draw_context, x, y + height - radius);
+	gral_draw_context_close_path(draw_context);
 }
 
 static void add_star(struct gral_draw_context *draw_context, float x, float y, float size) {
@@ -37,6 +71,8 @@ static void draw(struct gral_draw_context *draw_context, void *user_data) {
 		{1.f, 0.f, 1.f, 0.f, 1.f}
 	};
 	gral_draw_context_fill_linear_gradient(draw_context, 410.f, 10.f, 590.f, 100.f, stops, 3);
+	add_circle(draw_context, 10.f, 210.f, 180.f);
+	add_rounded_rectangle(draw_context, 210.f, 210.f, 180.f, 180.f, 20.f);
 	add_star(draw_context, 410.f, 210.f, 180.f);
 	gral_draw_context_fill(draw_context, 0.f, 0.f, 1.f, 1.f);
 }
