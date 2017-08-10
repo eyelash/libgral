761d0a6bc209acb030d33f650ffaee387f7dfc13
Elias Aebi <eyelash@users.noreply.github.com>
2017-08-10 09:10:25 +0200

    demo improvements

--- demos/audio.c
+++ demos/audio.c
@@ -2,7 +2,7 @@
 
 #define FREQUENCY 220.f
 
-#define FLOAT_TO_INT16(x) ((int16_t)((x) * 65535.f + .5f))
+#define INT16(x) ((int16_t)((x) * 32767.f + .5f))
 
 static float saw(float x) {
 	return x < .5f ? x * 2.f : x * 2.f - 2.f;
@@ -15,10 +15,10 @@ static int callback(int16_t *buffer, int frames) {
 	if (played_frames + frames > 44100 * 5) frames = 44100 * 5 - played_frames;
 	int t;
 	for (t = 0; t < frames; t++) {
-		float value = saw(x) * .2f;
+		float value = saw(x) * .4f;
 		float position = saw(position_x) * .5f + .5f;
-		buffer[2*t] = FLOAT_TO_INT16(value * (1.f - position));
-		buffer[2*t+1] = FLOAT_TO_INT16(value * position);
+		buffer[2*t] = INT16(value * (1.f - position));
+		buffer[2*t+1] = INT16(value * position);
 		x += FREQUENCY / 44100.f;
 		if (x > 1.f) x -= 1.f;
 		position_x += 1.f / 44100.f;
