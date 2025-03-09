897fd91943e8c154050ef298f4b0a6c2a837df57
Elias Aebi <eyelash@users.noreply.github.com>
2025-03-09 13:56:23 +0100

    macos: translate key codes

--- gral_macos.m
+++ gral_macos.m
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2016-2024 Elias Aebi
+Copyright (c) 2016-2025 Elias Aebi
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -367,7 +367,7 @@ void gral_draw_context_draw_transformed(struct gral_draw_context *draw_context, 
     WINDOW
  ===========*/
 
-static int get_key(UInt16 key_code) {
+static int get_key(unsigned short key_code) {
 	switch (key_code) {
 	case kVK_Return:
 		return GRAL_KEY_ENTER;
@@ -413,6 +413,67 @@ static int get_key(UInt16 key_code) {
 			}
 			return 0;
 		}
+	}
+}
+static int get_key_code(unsigned short key_code) {
+	switch (key_code) {
+	case 0x00: return 0x1E;
+	case 0x01: return 0x1F;
+	case 0x02: return 0x20;
+	case 0x03: return 0x21;
+	case 0x04: return 0x23;
+	case 0x05: return 0x22;
+	case 0x06: return 0x2C;
+	case 0x07: return 0x2D;
+	case 0x08: return 0x2E;
+	case 0x09: return 0x2F;
+	case 0x0B: return 0x30;
+	case 0x0C: return 0x10;
+	case 0x0D: return 0x11;
+	case 0x0E: return 0x12;
+	case 0x0F: return 0x13;
+	case 0x10: return 0x15;
+	case 0x11: return 0x14;
+	case 0x12: return 0x02;
+	case 0x13: return 0x03;
+	case 0x14: return 0x04;
+	case 0x15: return 0x05;
+	case 0x16: return 0x07;
+	case 0x17: return 0x06;
+	case 0x18: return 0x0D;
+	case 0x19: return 0x0A;
+	case 0x1A: return 0x08;
+	case 0x1B: return 0x0C;
+	case 0x1C: return 0x09;
+	case 0x1D: return 0x0B;
+	case 0x1E: return 0x1B;
+	case 0x1F: return 0x18;
+	case 0x20: return 0x16;
+	case 0x21: return 0x1A;
+	case 0x22: return 0x17;
+	case 0x23: return 0x19;
+	case 0x24: return 0x1C;
+	case 0x25: return 0x26;
+	case 0x26: return 0x24;
+	case 0x27: return 0x28;
+	case 0x28: return 0x25;
+	case 0x29: return 0x27;
+	case 0x2A: return 0x2B;
+	case 0x2B: return 0x33;
+	case 0x2C: return 0x35;
+	case 0x2D: return 0x31;
+	case 0x2E: return 0x32;
+	case 0x2F: return 0x34;
+	case 0x30: return 0x0F;
+	case 0x31: return 0x39;
+	case 0x32: return 0x29;
+	case 0x33: return 0x0E;
+	case 0x35: return 0x01;
+	case 0x7B: return 0x69;
+	case 0x7C: return 0x6A;
+	case 0x7D: return 0x6C;
+	case 0x7E: return 0x67;
+	default: return 0;
 	}
 }
 static int get_modifiers(NSEventModifierFlags modifier_flags) {
@@ -539,14 +600,14 @@ static int get_modifiers(NSEventModifierFlags modifier_flags) {
 	unsigned short key_code = [event keyCode];
 	int key = get_key(key_code);
 	if (key) {
-		interface->key_press(key, key_code, get_modifiers([event modifierFlags]), user_data);
+		interface->key_press(key, get_key_code(key_code), get_modifiers([event modifierFlags]), user_data);
 	}
 }
 - (void)keyUp:(NSEvent *)event {
 	unsigned short key_code = [event keyCode];
 	int key = get_key(key_code);
 	if (key) {
-		interface->key_release(key, key_code, user_data);
+		interface->key_release(key, get_key_code(key_code), user_data);
 	}
 }
 // NSTextInputClient implementation
